01ee5db6627e3aa004a3c8e6153f305c2185025c
minghui4 <mhming1103@gmail.com>
2023-11-05 03:01:16 +0800

    update layout of CampaginManagementPage

--- src/app/CampaginManagementPage/page.tsx
+++ src/app/CampaginManagementPage/page.tsx
@@ -97,10 +97,11 @@ const CampaginManagementPage = () => {
       alert(`Error: ${e}`);
     }
   }, [ManagerAddress,EventName,Description,Deadline,TargetFundsRaised, signer]);
-  return (
-    <div className={`relative flex flex-col items-center justify-center min-h-screen overflow-hidden ${styles.parent}`}>
-      <div className={`${styles.formContainer} w-full p-6 bg-white rounded-md shadow-lg lg:max-w-xl`}>
-        <div className="flex justify-between mb-4">
+
+return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen bg-gray-100 overflow-hidden">
+      <div className="w-full p-6 bg-white rounded-md shadow-lg lg:max-w-xl">
+        <div className="flex justify-between mb-8">
           <button 
               className="w-1/2 mr-2 py-2 tracking-wide text-white transition-colors transform bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
               onClick={(event) => setCreateCampagin(true)}
@@ -112,126 +113,120 @@ const CampaginManagementPage = () => {
               className="w-1/2 ml-2 py-2 tracking-wide text-white transition-colors transform bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:bg-red-700"
               onClick={(event) => setCreateCampagin(false)}
               >
-              
               End Campaign
           </button>
         </div>
         {createCampagin==true ? (
           <div>
-            <h1 className="text-3xl font-bold text-center text-gray-700 mb-6">Create a New Campaign</h1>
-            <form className="mt-6" onSubmit={createCampaginSubmit}>
-              <div className="mb-4">
+            <h1 className="text-5xl font-semibold text-center text-gray-700 mb-8">Create a New Campaign</h1>
+            <form className="mt-6 space-y-4" onSubmit={createCampaginSubmit}>
+              <div>
                 <label
                     htmlFor="ManagerAddress"
-                    className="block text-sm font-semibold text-gray-800"
+                    className="block text-lg font-semibold text-gray-800"
                 >
                 Manager&apos; Address
                 </label>
                 <input
                   type="text"
                   id="ManagerAddress"
-                  // value={}
                   onChange={(event) => setManagerAddress(event.target.value)}
-                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-gray-200 border-2 border-gray-300 rounded-md focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none focus:ring focus:ring-opacity-50"
                 />
               </div> 
-              <div className="mb-4">
+              <div>
                   <label
                       htmlFor="eventName"
-                      className="block text-sm font-semibold text-gray-800"
+                      className="block text-lg font-semibold text-gray-800"
                   >
                   Event Name
                   </label>
                   <input
                     type="text"
                     id="eventName"
-                    // value={}
                     onChange={(event) => setEventName(event.target.value)}
-                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-gray-200 border-2 border-gray-300 rounded-md focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none focus:ring focus:ring-opacity-50"
                   />
               </div> 
-              <div className="mb-2">
+              <div>
                 <label
                     htmlFor="Description"
-                    className="block text-sm font-semibold text-gray-800"
+                    className="block text-lg font-semibold text-gray-800"
                 >
                     Event Description
                 </label>
                 <input
                   type="text"
                   id="EventDescription"
-                  // value={ApproverAddress}
                   onChange={(event) => setDescription(event.target.value)}
-                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-gray-200 border-2 border-gray-300 rounded-md focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none focus:ring focus:ring-opacity-50"
                 />
               </div>
-              <div className="mb-2">
+              <div>
                 <label
                     htmlFor="Event Deadline"
-                    className="block text-sm font-semibold text-gray-800"
+                    className="block text-lg font-semibold text-gray-800"
                 >
                     Event Deadline
                 </label>
                 <input
-                  // type="number"
                   type="date"
                   id="EventDeadline"
-                  // value={ApproverAddress}
                   onChange={(event) => setDeadline(Math.floor(new Date(event.target.value).getTime() / 1000))}
-                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-gray-200 border-2 border-gray-300 rounded-md focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none focus:ring focus:ring-opacity-50"
                 />
               </div>
-              <div className="mb-2">
+              <div>
                 <label
                     htmlFor="FundRaising Target"
-                    className="block text-sm font-semibold text-gray-800"
+                    className="block text-lg font-semibold text-gray-800"
                 >
                     Target Fund Raising
                 </label>
                 <input
                   type="number"
                   id="FundRaisingTarget"
-                  // value={FundRaisingTarget}
                   onChange={(event) => setTargetFundsRaised(event.target.valueAsNumber)}
-                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-gray-200 border-2 border-gray-300 rounded-md focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none focus:ring focus:ring-opacity-50"
                 />
               </div>
-              <button 
-                  className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
-                  type="submit"
-                  onChange={(event) => setCreateCampagin(true)}
-                  >
-                  Create New Campaign
-              </button>
+              <div className="mt-2">
+                <button 
+                    className="w-full px-4 py-2 text-lg tracking-wide text-white transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
+                >
+                  Submit
+                </button>
+              </div>
             </form>
           </div>
-        ):(
-          <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
-            <h1 className="text-3xl font-bold text-center text-gray-700 mb-6">End a Campaign</h1>
-            <form className="mt-6" onSubmit={endCampaginSubmit}>
-              <div className="mb-4">
-                  <label
-                      htmlFor="Manager's Name"
-                      className="block text-sm font-semibold text-gray-800"
-                  >
-                  Campaign ID
-                  </label>
-                  <input
-                    type="text"
-                    id="ManagerName"
-                    // value={}
-                    onChange={(event) => setcampaignId(ethers.utils.toUtf8Bytes(event.target.value))}
-                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                  />
+        ) : (
+          <div>
+            <h1 className="text-5xl font-semibold text-center text-gray-700 mb-8">End Campaign</h1>
+            <form className="mt-6 space-y-4" onSubmit={endCampaginSubmit}>
+              <div>
+                <label
+                    htmlFor="ContractAddress"
+                    className="block text-lg font-semibold text-gray-800"
+                >
+                  Contract Address
+                </label>
+                <input
+                  type="text"
+                  id="ContractAddress"
+                  onChange={(event) => setContractAddress(event.target.value)}
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-gray-200 border-2 border-gray-300 rounded-md focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none focus:ring focus:ring-opacity-50"
+                />
               </div> 
-              <button 
-                  className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
-                  type="submit">
-                  End Campaign 
-              </button>
+              <div className="mt-2">
+                <button 
+                    className="w-full px-4 py-2 text-lg tracking-wide text-white transition-colors duration-200 transform bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:bg-red-700"
+                >
+                  Submit
+                </button>
+              </div>
             </form>
           </div>
-        )} 
+        )}
       </div>
     </div>
   );
