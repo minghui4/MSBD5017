4ad6bd1e5e453cecf676ae7faf05590ec5494923
minghui4 <mhming1103@gmail.com>
2023-11-11 02:13:42 +0800

    revised the name

--- src/app/donation/api/checkout.js
+++ src/app/donation/api/checkout.js
@@ -6,6 +6,7 @@ export async function checkout({lineItems}){
 	const getStripe = () => {
 		if(!stripePromise) {
 			stripePromise = loadStripe(process.env.NEXT_PUBLIC_API_KEY)
+			
 		}
 		return stripePromise
 	}
